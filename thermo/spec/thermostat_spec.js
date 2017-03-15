2fe924a5b45f657113d26ab6a78e02fd99c990bc
Nicholas Leacock <nicholas@swissmail.com>
2017-03-15 22:23:50 +0000

    Added Power Saver with tests

--- thermo/spec/thermostat_spec.js
+++ thermo/spec/thermostat_spec.js
@@ -1,6 +1,5 @@
 describe("Thermostat", function() {
   var thermostat;
-  var temperature;
 
   beforeEach(function(){
     thermostat = new Thermostat();
@@ -10,10 +9,29 @@ describe("Thermostat", function() {
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
 
-  it("raises the temperature by 5 degrees", function(){
-    thermostat.increaseTemperature(5)
-    expect(thermostat.getCurrentTemperature()).toEqual(25);
+  it("raises the temperature by 1 degree", function(){
+    thermostat.increaseTemperature(1)
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
   });
 
+  it("decreases the temperature by 1 degree", function(){
+    thermostat.decreaseTemperature(1)
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  describe("It throws an error", function(){
+
+    it("if temperature falls below 10 degrees", function() {
+      // for (var i = 15; i > 1; i--) {}
+      thermostat.temperature = 12
+      expect(function(){thermostat.decreaseTemperature(3);}).toThrow("The temperature cannot fall below 10 degrees");
+    });
+
+    it("if temperature rises above 25 degrees when power saving is on", function(){
+      thermostat.temperature = 25
+      expect(function(){thermostat.increaseTemperature(1);}).toThrow("Power Saving Mode is on. Temperature cannot rise above 25 degrees.");
+    });
+
+  });
 
 });
--- thermo/src/thermostat.js
+++ thermo/src/thermostat.js
@@ -3,10 +3,23 @@ function Thermostat() {
   this.temperature = 20;
 };
 
+var powerSaver
+powerSaver = true
+
 Thermostat.prototype.getCurrentTemperature = function(){
  return this.temperature;
 };
 
 Thermostat.prototype.increaseTemperature = function(number){
   this.temperature += number;
+  if (this.temperature > 25 && powerSaver == true){
+    throw "Power Saving Mode is on. Temperature cannot rise above 25 degrees."
+    }
+};
+
+Thermostat.prototype.decreaseTemperature = function(number){
+  this.temperature -= number;
+  if (this.temperature < 10){
+  throw("The temperature cannot fall below 10 degrees");
+  }
 };
